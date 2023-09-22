fb6a7e96ac69e8238c259cfbe55f118d089ea2d5
MathGiraud <mgiraud@millesima.com>
2023-09-22 13:33:52 +0200

    MAJ userContoller with try catch

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,60 +4,73 @@ import bcrypt from 'bcrypt';
 const userController = {
     login: async (req, res) => {
         const  {email, password } = req.body;
-        const user = await User.findOne({where: { email }});
-        if (!user) {
-            return res.status(401).json({message: "email ou mot de passe incorrect"});
+        try {
+            const user = await User.findOne({where: { email }});
+            if (!user) {
+                return res.status(401).json({message: "email ou mot de passe incorrect"});
+            }
+            const validPassword = await bcrypt.compare(password, user.password);
+            if (!validPassword) {
+                return res.status(401).json({message: "email ou mot de passe incorrect"});
+            }
+            res.status(200).json({ message: "Connexion réussie", user});
+        } catch (error) {
+            res.status(500).render('error');
         }
-        const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) {
-            return res.status(401).json({message: "email ou mot de passe incorrect"});
-        }
-        res.status(200).json({ message: "Connexion réussie", user});
     },
 
-    getUsers: (req, res) => {
-        User.findAll({
-            attributes : {exclude : ["createdAt", "updatedAt"]}
-        })
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(error => res.status(500).json(error))
+    getUsers: async (req, res) => {
+        try{
+            const users = await User.findAll({attributes : {exclude : ["createdAt", "updatedAt"]}})
+            if(users) {
+                return res.status(200).json(users);
+            } else {
+                return res.status(500).json({ message: "Users non retournés"});
+            }
+        } catch (error) {
+            res.status(500).json({message : 'default in Users route', error: error});
+        }
     },
     
     getUserById: async (req, res) => {
         const userId = parseInt(req.params.id, 10);
-
-        User.findByPk(userId, {
-            attributes : {exclude : ["createdAt", "updatedAt"]},
-            include: ['role']
-        })
-        .then(user => {
-            res.status(200).json(user);
-        })
-        .catch(error => res.status(500).json(error))
+        try {
+            const user = await User.findByPk(userId, {
+                attributes : {exclude : ["createdAt", "updatedAt"]},
+                include: ['role']
+            });
+            if(user) {
+                return res.status(200).json(user);
+            } else {
+                return res.status(404).json('User not found');
+            }
+        } catch (error) {
+            return res.status(500).json({message : 'default in User route', error: error});
+        }
     },
 
     createUser: async (req, res) => {
         const { body } = req;
-        const hashedPassword = await bcrypt.hash(body.password, 10);
-        User.create({ ...body, password: hashedPassword })
-        .then(() => {
-            res.status(201).json({message : "Utilisateur créé"});
-        })
-        .catch(error => res.status(500).json(error))
+        try {
+            const hashedPassword = await bcrypt.hash(body.password, 10);
+            const user = await User.create({ ...body, password: hashedPassword })
+            return res.status(201).json({message : "Utilisateur créé"});
+        } catch (error) {
+            return res.status(500).json({message : 'default in User Creation route', error: error});
+        }
     },
 
     updateUser: async(req, res) => {
         const userId = parseInt(req.params.id, 10);
-        const userToUpdate = await User.findByPk(userId);
-        
-        const { body } = req;
-        userToUpdate.update({...body})
-        .then(() => {
-            res.status(201).json({message : "Utilisateur modifié"});
-        })
-        .catch(error => res.status(500).json(error))
+        try {
+            const userToUpdate = await User.findByPk(userId);
+            const { body } = req;
+            await userToUpdate.update({...body});
+            res.status(201).json({message : "Utilisateur modifié", user: userToUpdate});
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
     },
 };
 
