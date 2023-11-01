47c2c0ddcbdaa0548f3f8c80dfbaf04d2f8c3ab8
MathGiraud <mgiraud@millesima.com>
2023-11-01 16:50:04 +0100

    add confirmation message and contact Admins

--- controllers/contactMailController.js
+++ controllers/contactMailController.js
@@ -1,54 +1,49 @@
-const {sendMailAnnouncement, createHTML} = require('../services/contactMail.js');
+const sendMail = require('../services/contactMail.js');
+const createHtmlResponseAnnouncement = require('../services/templatesHtml/ResponseAnouncement.js');
+const createHtmlResponseConfirmation = require('../services/templatesHtml/ConfirmationSendedMail.js');
+const createHtmlContactAdmin = require('../services/templatesHtml/ContactAdmins.js');
 require('dotenv').config();
 const announcementController = require('./announcementController.js');
 
 const contactController = {
 
     contactAdmin: async (req, res) => {
-        try{
-            const to = process.env.EMAIL;
-            const { subject, text } = req.body;
-            await sendMail(to, subject, text);
-            
-            res.status(200).send({success: true, message: 'Email envoyé !'});
-
-        } catch (error) {
-            res.status(500).send({ success: false, message: 'Error !', error: error.message });
+        //! Pour l'instant me manque l'info de savoir si j'ai un user ou nous ... 
+        //! me faut une fonction de check du accessToken ... 
+        //! sans qu'elle bloque la progression.
+        const to = process.env.EMAIL;
+        const { subject, text } = req.body;
+        if (req.user) {
+            const htmlToAdmin = createHtmlContactAdmin(req.user, text);
+            await sendMail(to, subject, htmlToAdmin);
+        } else {
+            const htmlToAdmin = createHtmlContactAdmin( "" ,text);
+            await sendMail(to, subject, htmlToAdmin);
         }
+            
+        if (req.user) {
+            const htmlConfirmation = createHtmlResponseConfirmation( "", req.user, text);
+            const subjectConfirmation = "Ton message a bien été envoyé !"
+            const toUser = req.user.email;
+            await sendMail(toUser, subjectConfirmation, htmlConfirmation);
+        }
+        res.status(200).send({success: true, message: 'Email envoyé !'});
     },
-
-    //! NEED :
-    // Annoucement User (pour récupérer son mail),
-    // Je le récupère avec l'id Annoucement pour donc à envoyer par le front
-    // -> j'ai mon user_id
-    // -> j'ai mon Subject a contruire avec le nom de l'annonce et autre ... 
-    
-    // Le Text : Je récupère le message 
-    // -> je construit mon message en inscrivant le mais du user qui répond à l'annonce. 
     
     contactAnnouncement: async (req, res) => {
         
         const announcementId = parseInt(req.body.announcement_id, 10);
         const announcement = await announcementController.findAnnouncementById(announcementId);
-        // console.log("MonUserMail", req.user.email);
-        // console.log("Mon Annonce", announcement.user);
 
+        //const to = announcement.user.email;
         const to = "mathgiraud33@gmail.com";
         const { subject, text } = req.body;
-        const html = createHTML(announcement, req.user, text);
-        await sendMailAnnouncement(to, subject, html);
-
+        const htmlAnnoucementResponse = createHtmlResponseAnnouncement(announcement, req.user, text);
+        await sendMail(to, subject, htmlAnnoucementResponse);
+        const htmlConfirmation = createHtmlResponseConfirmation(announcement, req.user, text);
+        const subjectConfirmation = "Ton message a bien été envoyé !"
+        await sendMail(to, subjectConfirmation, htmlConfirmation);
         return res.status(200).send({success: true, message: 'Email envoyé !'});
-//         try{
-//             const to = process.env.EMAIL;
-//             const { subject, text } = req.body;
-//             await sendMail(to, subject, text);
-            
-//             res.status(200).send({success: true, message: 'Email envoyé !'});
-
-//         } catch (error) {
-//             res.status(500).send({ success: false, message: 'Error !', error: error.message });
-//         }
     },
 };
 
---
+++ services/templatesHtml/ResponseAnouncement.js
@@ -0,0 +1,103 @@
+require('dotenv').config();
+
+
+const createHtmlResponseAnnouncement = (announcement, user, text) => {
+    const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <style>
+            .h1 {
+                color: #CBE4DE;
+                font-family: "Vampiro One", sans-serif; 
+                margin: 0;
+            }
+            .h2 {
+                color: black;
+            }
+            .h3 {
+                color: black;
+            }
+            .h4 {
+                color: black;
+            }
+            .h5 {
+                color: black;
+            }
+            .p {
+                color: black;
+            }
+        </style>
+    </head>
+        <body style="font-family: "Montserrat", sans-serif; background-color: #fff; margin: 0; padding: 0;">
+            <table width="100%" cellspacing="0" cellpadding="0" border="0" style="background-color: #fff;">
+                <tr>
+                <td align="center" style="padding: 20px;">
+                    <table width="600" cellspacing="0" cellpadding="0" border="0" style="background-color: #CBE4DE; border-radius: 10px;">
+                    <tr>
+                        <td>
+            
+                        <!-- En-tête avec logo et nom -->
+                        <table width="100%" cellspacing="0" cellpadding="0" border="0" style="background-color: #0E8388; border-radius: 10px 10px 0 0;">
+                            <tr>
+                            <td style="padding: 20px; display: flex; align-items: center;">
+                                <img src="cid:logo" width="65" height="65" alt="Logo O'my band" style="margin-right: 10px;" />
+                                <h1 style="font-size: 1.5rem; color: #CBE4DE; font-family: 'Vampiro One', sans-serif; margin: 0;">O'MY BAND</h1>
+                            </td>
+                            <td align="right" style="color: #CBE4DE; padding: 20px;">
+                                <h3>Bonjour ${announcement.user.pseudo} !</h3>
+                            </td>
+                            </tr>
+                        </table>
+            
+                        <!-- Contenu du corps de l'e-mail -->
+                        <table width="100%" cellspacing="0" cellpadding="20" border="0">
+                            <tr style="background-color: #fff; width: 100%;">
+                                <td colspan="3">
+                                    <h4>Tu as reçu une réponse concernant l'annonce suivante :</h4>
+                                </td>
+                            </tr>
+                            <tr style="border-top: 10px solid #CBE4DE; border-bottom: 10px solid #CBE4DE;">
+                                <td style="background-color: #fff; width: 10%;"></td>
+                                <td>
+                                    <h2 style="color: #0E8388; margin-top: 0;">${announcement.title}</h2>
+                                    <p style="margin: 0;">Description de l'annonce : ${announcement.description}</p>
+                                </td>
+                                <td style="background-color: #fff; width: 10%;"></td>
+                            </tr>
+                            <tr style="background-color: #fff; width: 100%;">
+                                <td colspan="3">
+                                    <h4>${user.pseudo} t'a envoyé ce message :</h4>
+                                    <p style="margin: 0;">${text}</p>
+                                    <p style="border-bottom: 1px solid black;"></p>
+                                    <p>${user.pseudo} vient de ${user.location} et se décrit comme ceci : </p>
+                                    <p>${user.description}</p>  
+                                </td>
+                            </tr>
+                        </table>
+            
+                        <!-- Pied de page avec liens -->
+                        <table width="100%" cellspacing="0" cellpadding="0" border="0" style="background-color: #0e8388; border-radius: 0 0 10px 10px;">
+                            <tr>
+                            <td align="left" style="padding: 10px 20px;">
+                                <a href="mailto:${user.email}" style="color: #CBE4DE; text-decoration: none; font-size: 16px; font-weight: bold;">Répondre</a>
+                            </td>
+                            <td align="right" style="padding: 10px 20px;">
+                                <a href="mailto:${process.env.EMAIL}" style="color: #CBE4DE; text-decoration: none; font-size: 14px;">Contacter les administrateurs</a>
+                            </td>
+                            </tr>
+                        </table>
+            
+                        </td>
+                    </tr>
+                    </table>
+                </td>
+                </tr>
+            </table>
+        </body>
+    </html>
+    `;
+    return htmlContent;
+}
+module.exports = createHtmlResponseAnnouncement ;
