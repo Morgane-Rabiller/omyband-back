047daddd99b48639fe623943075d3f13a79067ef
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-03-14 10:38:00 +0100

    improvement of authorize method

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,43 @@
-const User = require( "../models/userModel.js");
-const jwt = require( 'jsonwebtoken');
-const bcrypt = require( 'bcrypt');
-require('dotenv').config()
-const sanitizeHtml = require('sanitize-html');
+const User = require("../models/userModel.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const sanitizeHtml = require("sanitize-html");
 
 const defaultOptionsSanitize = {
     allowedTags: [],
-    allowedAttributes: {}
-}
+    allowedAttributes: {},
+};
 
 const authController = {
     login: async (req, res) => {
-        req.body.email = sanitizeHtml(req.body.email, defaultOptionsSanitize)
-        req.body.password = sanitizeHtml(req.body.password, defaultOptionsSanitize)
-        
-        const {email, password} = req.body
+        req.body.email = sanitizeHtml(req.body.email, defaultOptionsSanitize);
+        req.body.password = sanitizeHtml(
+            req.body.password,
+            defaultOptionsSanitize
+        );
 
-        const user = await User.findOne({where: { email }});
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
         if (!user) {
-            return res.status(401).json({message: "email ou mot de passe incorrect"});
+            return res
+                .status(401)
+                .json({ message: "email ou mot de passe incorrect" });
         }
         if (await bcrypt.compare(password, user.password)) {
             return authController.sendToken(res, user);
-        };
-        return res.status(401).json({message: "email ou mot de passe incorrect"});
+        }
+        return res
+            .status(401)
+            .json({ message: "email ou mot de passe incorrect" });
     },
 
     async sendToken(res, user) {
         const accessToken = await authController.generateAccessToken(user);
-        return res.status(200).json({ message: "Connexion réussie", accessToken});
+        return res
+            .status(200)
+            .json({ message: "Connexion réussie", accessToken });
     },
 
     async generateAccessToken(user) {
@@ -44,48 +53,53 @@ const authController = {
                     description: user.description,
                 },
             },
-            process.env.JWT_SECRET,
+            process.env.JWT_SECRET
             // { expiresIn: '3h'}
         );
     },
 
-    // Middleware d'authorisation pour les routes protégées. 
+    // Middleware d'authorisation pour les routes protégées.
     // Hearder necessaire : authorization
     // Format : "bearer + ' ' + accessToken"
     authorize: async (req, res, next) => {
-        const header = req.headers['authorization'];
-    
-        const accessToken = header.split(' ')[1];
-
         try {
-            const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET);
-        req.user = decodedAccessToken.data;
-            next()
-        } catch(error) {
-            return res.status(401).json({message: "Aucun Token d'authorisation fourni"});
+            const header = req.headers["authorization"];
+
+            const accessToken = header.split(" ")[1];
+
+            const decodedAccessToken = jwt.verify(
+                accessToken,
+                process.env.JWT_SECRET
+            );
+            req.user = decodedAccessToken.data;
+            next();
+        } catch (error) {
+            return res
+                .status(401)
+                .json({ message: "Aucun Token d'authorisation fourni" });
         }
     },
 
     addTokenUser: (req) => {
-        const header = req.headers['authorization'];
+        const header = req.headers["authorization"];
         if (!header) {
-            return req.user = "";
+            return (req.user = "");
         }
-        const accessToken = header.split(' ')[1];
+        const accessToken = header.split(" ")[1];
         if (!accessToken) {
-            return req.user = "";
+            return (req.user = "");
         }
         try {
-            const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET);
+            const decodedAccessToken = jwt.verify(
+                accessToken,
+                process.env.JWT_SECRET
+            );
             req.user = decodedAccessToken.data;
-            return req.user = decodedAccessToken.data;
+            return (req.user = decodedAccessToken.data);
         } catch (error) {
-            return req.user = "";
+            return (req.user = "");
         }
-        
-        
     },
 };
-
 
 module.exports = authController;
